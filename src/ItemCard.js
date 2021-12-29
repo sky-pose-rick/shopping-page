a16cc718c7766061b64d85ad20919a20e143530b
Richard Cranwell <rickycranwell@gmail.com>
2021-12-29 13:05:48 -0600

    create ItemCard component to display shop items

---
+++ src/ItemCard.js
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+
+function ItemCard(props) {
+  const {
+    title, src, alt, onSubmit,
+  } = props;
+  const [qty, setQty] = useState(1);
+
+  const increase = () => {
+    setQty(qty + 1);
+  };
+
+  const decrease = () => {
+    setQty(qty - 1);
+  };
+
+  const onChange = (e) => {
+    // console.log(e.target.value);
+    const inputNum = Math.floor(Number(e.target.value));
+    setQty(inputNum > 0 ? inputNum : 0);
+  };
+
+  return (
+    <div className="ItemCard">
+      <h3>{title}</h3>
+      <img src={src} alt={alt} />
+      <button type="button" onClick={decrease} disabled={qty <= 0}>-</button>
+      <input type="text" onChange={(e) => onChange(e)} value={qty} />
+      <button type="button" onClick={increase}>+</button>
+      <button type="submit" onClick={() => { onSubmit(qty); }}>-</button>
+    </div>
+  );
+}
+
+export default ItemCard;
---
+++ src/ItemCard.test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ItemCard from './ItemCard';
+
+describe('', () => {
+  const mockSubmit = jest.fn((a) => a);
+
+  beforeEach(() => {
+    render(<ItemCard
+      title="item1"
+      src="item1.png"
+      alt="first item"
+      onSubmit={mockSubmit}
+    />);
+  });
+  it('item card renders', () => {
+    const image = screen.getByRole('img');
+    expect(image).toBeInTheDocument();
+  });
+
+  it('reads values from props', () => {
+    const image = screen.getByRole('img');
+    expect(image.src).toMatch(/item1.png/);
+    expect(image.alt).toEqual('first item');
+    expect(screen.getByText(/item1/i)).toBeInTheDocument();
+  });
+
+  it('has a text input and 3 buttons', () => {
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+    const buttons = screen.getAllByRole('button');
+    expect(buttons.length).toBe(3);
+  });
+
+  it('onSubmit is called by final button', () => {
+    const buttons = screen.getAllByRole('button');
+    fireEvent.click(buttons[2]);
+    expect(mockSubmit).toBeCalled();
+  });
+
+  it('textbox and +/- buttons work', () => {
+    const buttons = screen.getAllByRole('button');
+    const text = screen.getByRole('textbox');
+    fireEvent.change(text, { target: { value: 5 } });
+    // +2, -1
+    fireEvent.click(buttons[1]);
+    fireEvent.click(buttons[1]);
+    fireEvent.click(buttons[0]);
+    // submit
+    fireEvent.click(buttons[2]);
+    expect(mockSubmit).toBeCalledWith(6);
+  });
+
+  it('- button disabled at 0', () => {
+    const buttons = screen.getAllByRole('button');
+    const text = screen.getByRole('textbox');
+    fireEvent.change(text, { target: { value: 0 } });
+    expect(buttons[0].disabled).toBe(true);
+  });
+
+  it.todo('test input validation');
+});
