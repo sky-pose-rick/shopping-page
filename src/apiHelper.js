536b4afc2df3b411eaf5f96997cf5e438f00fc0b
Richard Cranwell <rickycranwell@gmail.com>
2021-12-31 15:40:12 -0600

    use giphy api to get data to create items

--- src/apiHelper.js
+++ src/apiHelper.js
@@ -1,6 +1,19 @@
-function getAPIData() {
-  const apiKey = 'find an api';
-  return Promise.resolve(['todo']);
+// get the top giphy stickers to use as items
+async function getAPIData() {
+  const key = 'ieAImKLCMr3MRyKct42s8BMVBEdSgL0A';
+  const limit = 20;
+
+  const response = await fetch(`https://api.giphy.com/v1/stickers/trending?api_key=${key}&limit=${limit}`, { mode: 'cors' });
+  const json = await (response.json());
+  const itemData = json.data.map((value) => {
+    const { title, id } = value;
+    const src = value.images.downsized_medium.url;
+    const alt = title;
+    return {
+      title, src, alt, id,
+    };
+  });
+  return Promise.resolve(itemData);
 }
 
 export default { getAPIData };
